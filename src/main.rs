a31eba197ad392efd88f9e92f3e3e058f58136c9
Abey Onalaja <Abey.onalaja@gmail.com>
2019-12-30 12:40:19 +0000

    update to change block colors

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
 use std::thread::sleep;
+use sdl2::render::{Canvas, Texture, TextureCreator};
+use sdl2::rect::Rect;
+use sdl2::video::{Window, WindowContext};
+
+#[derive(Clone, Copy)]
+enum TextureColor {
+    Green,
+    Blue,
+}
 
 pub fn main() {
     let sdl_context = sdl2::init().expect("SDL initialization failed");
@@ -16,10 +25,35 @@ pub fn main() {
         .build()
         .expect("Failed to create window");
 
-    let mut canvas = window.into_canvas().build().expect("Failed to convert window to canvas");
+//    let mut canvas = window.into_canvas().build().expect("Failed to convert window to canvas");
+
+    let mut canvas = window.into_canvas()
+        .target_texture()
+        .present_vsync()
+        .build()
+        .expect("Couldnt get windows canvas\
+        ");
+
+
+
+
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
+    const TEXTURE_SIZE: u32 = 32;
+    let mut square_texture: Texture =
+    texture_creator.create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE)
+        .expect("Failed to create a texture");
+
+    canvas.with_texture_canvas(&mut square_texture, |texture|{
+        texture.set_draw_color(Color::RGB(0, 255, 0));
+        texture.clear();
+    });
+
+
 
     canvas.set_draw_color(Color::RGB(255,0,0));
     canvas.clear();
+    canvas.copy(&square_texture, None, Rect::new(0,0, TEXTURE_SIZE, TEXTURE_SIZE))
+        .expect("Couldnt copy texture to window");
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().expect("Failed to get SDL event pump");
@@ -38,3 +72,5 @@ pub fn main() {
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
+
+fn create_texture_rect<'a>(Canvas: &mut Canvas<Window>, texture_creator: &'a TextureCreator)
