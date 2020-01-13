de964041f92f7761d46ed1e5b10fbf71e7581c2f
Abey Onalaja <Abey.onalaja@gmail.com>
2020-01-13 00:05:25 +0000

    create new tetrimino's

--- src/main.rs
+++ src/main.rs
@@ -188,6 +188,27 @@ impl TetriminoGenerator for TetriminoT {
     }
 }
 
+fn create_new_tetrimino() -> Tetrimino{
+    static mut PREV: u8 =7;
+    let mut rand_nb = rand::random::<u8>() % 7;
+
+    if unsafe {PREV} == rand_nb {
+        rand_nb = rand::random::<u8>() % 7;
+    }
+
+    unsafe {PREV = rand_nb;}
+
+    match rand_nb {
+        0 =>TetriminoI::new(),
+        1 => TetriminoJ::new(),
+        2 => TetriminoL::new(),
+        3 => TetriminoO::new(),
+        4 => TetriminoS::new(),
+        5 => TetriminoZ::new(),
+        6 => TetriminoT::new(),
+        _ => unreachable!(),
+    }
+}
 
 
 pub fn main() {
